9a1466d28e596d52bd7f7683a2d317a7657c581e
Yorman Rojas <yormanxrojas@gmail.com>
2024-02-23 18:27:00 -0600

    UPDATED: Changes in authentication, layout, buttons, forms, SVG icons, index.html, and styles.scss.
    - Updated authentication.component.html:
      - Added class "w-full" to input elements.
    - Updated authentication.component.scss:
      - Added height and flex properties to authentication-wrapper.
      - Added padding to authentication-content.
      - Added class "w-full" to input elements and change-mode-button.
    - Updated home.component.html:
      - Commented out status-wrapper div.
    - Updated layout.component.html:
      - Added aside, section, and button elements.
      - Removed header and main elements.
    - Updated layout.component.scss:
      - Added styles for aside, main, search-bar-wrapper, and router-outlet-wrapper.
    - Updated layout.component.ts:
      - Added resize event listener to check window width.
      - Updated closeSession() method to signOut().
    - Updated button-link.scss:
      - Removed class "w-full".
    - Updated button-primary.scss:
      - Removed class "w-full".
    - Updated inputs.scss:
      - Removed class "w-full" and "mb-4".
    - SVG icons: check-circle.svg, clipboard.svg, clock.svg, home-line.svg, loading-01.svg, plus.svg, search-lg.svg, and x-close.svg.
    - Updated index.html:
      - Added class "m-0 antialiased" to body.
    - Updated styles.scss:
      - Added background gradient and fixed background attachment to html and body.

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { StorageService } from '../core/services/storage.service';
 import { Router, RouterModule } from '@angular/router';
+import { filter, fromEvent, map } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -14,13 +15,22 @@ import { Router, RouterModule } from '@angular/router';
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
-  
+
   constructor(
     private storageService: StorageService,
     private router: Router
-  ){}
+  ) {
+    fromEvent(window, 'resize')
+      .pipe(
+        filter(() => window.innerWidth < 1500),
+        map(() => window.innerWidth)
+      )
+      .subscribe((width) => {
+        console.log(`Window width is now less than 1500: ${width}`);
+      });
+  }
 
-  closeSession(){
+  signOut() {
     alert("Goodbye");
     this.storageService.removeSessionItem('jwt');
     this.router.navigate(['/authentication']);
